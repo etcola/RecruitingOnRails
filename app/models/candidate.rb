07a5e3b906cc7e6567f66a9e6e2011d40eac85d7
Mao Chao <chaomao@thoughtworks.com>
2012-02-04 01:30:17 +0800

    validate file size

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,11 +1,11 @@
 # encoding: UTF-8
+require 'file_size_validator'
 class Candidate < ActiveRecord::Base
   validates_presence_of :name, :message => "姓名不能为空"
 
   validates_presence_of :cellphone, :message => "手机号码不能为空"
   validates_numericality_of :cellphone, :message => "手机号码包含非数字", :unless => :error_for_cellphone?
   validates_uniqueness_of :cellphone, :scope => :email, :message => "手机号码已存在", :unless => :error_for_cellphone?
-  validates_uniqueness_of :email, :scope => :cellphone, :message => "邮箱地址已存在", :unless => :error_for_email?
   validates_length_of :cellphone, :is=>11, :message => "手机号码长度不是11位", :unless => :error_for_cellphone?
 
   validates_presence_of :sex, :message => "性别不能为空"
@@ -16,14 +16,17 @@ class Candidate < ActiveRecord::Base
 
   validates_presence_of :email, :message => "邮箱不能为空", :unless => :error_for_email?
   validates_format_of :email, :with=>/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :message => "邮箱格式不正确", :unless => :error_for_email?
+  validates_uniqueness_of :email, :scope => :cellphone, :message => "邮箱地址已存在", :unless => :error_for_email?
 
   validates_presence_of :degree, :message => "学位不能为空"
   validates_presence_of :job_title, :message => "应聘职位不能为空"
   validates_presence_of :track_source, :message => "信息来源不能为空"
 
-  validates_presence_of :resume, :message => "请指定要上传的简历"
-
   mount_uploader :resume, ResumeUploader
+  validates_presence_of :resume, :message => "简历不能为空"
+  validates :resume, :presence => true, :file_size => {
+      :maximum => 2.megabytes.to_i
+  }
 
   private
 
