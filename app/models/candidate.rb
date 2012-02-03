de86e67a90492f9cb062c3e15ca035d224110cd0
Suo Qin <suo.qin@rea-group.com>
2012-02-03 14:06:13 +0800

    add validation during new candidate, fix typo ThoughtWorks

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,29 @@
+# encoding: UTF-8
 class Candidate < ActiveRecord::Base
+  validates_presence_of :name, :message => "姓名不能为空"
+
+
+  validates_presence_of :cellphone, :message => "手机号码不能为空"
+  validates_numericality_of :cellphone, :message => "手机号码包含非数字", :unless => :error_for_cellphone?
+  validates_uniqueness_of :cellphone, :message => "手机号码已存在", :unless => :error_for_cellphone?
+  validates_length_of :cellphone, :is=>11, :message => "手机号码长度不是11位", :unless => :error_for_cellphone?
+
+  validates_presence_of :sex, :message => "性别不能为空"
+  validates_presence_of :school, :message => "学校名称不能为空"
+  validates_presence_of :major, :message => "专业不能为空"
+  validates_presence_of :graduate_year, :message => "毕业年份不能为空"
+  validates_presence_of :graduate_month, :message => "毕业月份不能为空"
+
+  validates_presence_of :email, :message => "邮箱不能为空", :unless => :error_for_email?
+  validates_format_of :email, :with=>/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :message => "邮箱格式不正确", :unless => :error_for_email?
+
+  private
+
+  def error_for_cellphone?
+    errors.has_key?(:cellphone)
+  end
+
+  def error_for_email?
+    errors.has_key?(:email)
+  end
 end
