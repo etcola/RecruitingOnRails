5b298b5c017e1bd19d9046a6088ac74578dd0373
Suo Qin <suo.qin@rea-group.com>
2012-02-03 10:46:04 +0800

    change language to Chinese

--- db/migrate/20120202140523_split_graduate.rb
+++ db/migrate/20120202140523_split_graduate.rb
@@ -2,12 +2,12 @@ class SplitGraduate < ActiveRecord::Migration
   def up
     add_column :candidates, :graduate_year, :string
     add_column :candidates, :graduate_month, :string
-    remove_column :candidates, :gradute_at
+    remove_column :candidates, :graduate_at
   end
 
   def down
     remove_column :candidates, :graduate_year
     remove_column :candidates, :graduate_month
-    add_column :candidates, :gradute_at, :date
+    add_column :candidates, :graduate_at, :date
   end
 end
