51fb6571062bea45640e4fed5cb2fd19a37d0f11
Mao Chao <chaomao@thoughtworks.com>
2012-02-03 19:18:47 +0800

    add cellphone number into uploaded file

--- app/uploaders/resume_uploader.rb
+++ app/uploaders/resume_uploader.rb
@@ -13,7 +13,11 @@ class ResumeUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}_#{model.id}"
+  end
+
+  def filename
+    "#{model.cellphone}_#{original_filename}"
   end
 
   def extension_white_list
