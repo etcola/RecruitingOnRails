749e47c678e67f7bb9c9267654fadfe5c7c38f18
Mao Chao <chaomao@thoughtworks.com>
2012-02-22 12:11:19 +0800

    fix typo

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-RecruitingOnRails::Application.config.session_store :cookie_store, key: '_RecruitingOnRails_session'
+RecruitingOnRails::Application.config.session_store :cookie_store, :key => '_RecruitingOnRails_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
